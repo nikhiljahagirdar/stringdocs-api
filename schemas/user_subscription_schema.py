3ce608782e37a075a4aa146721ff777ea62faa78
Nikhil Jahagirdar <nikhil.jahagirdar@gmail.com>
2025-08-11 17:21:39 +0530

    Update user_subscription_schema.py

--- schemas/user_subscription_schema.py
+++ schemas/user_subscription_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class UserSubscriptionBase(BaseModel):
     user_id: int
     subscription_id: int
@@ -10,11 +11,29 @@ class UserSubscriptionBase(BaseModel):
     start_date: datetime
     end_date: datetime | None = None
 
-class UserSubscriptionCreate(UserSubscriptionBase):
-    pass
+
+class UserSubscriptionCreate(BaseModel):
+    user_id: int
+    subscription_id: int
+    stripe_customer_id: str
+    stripe_subscription_id: str
+    status: str
+    start_date: datetime
+    end_date: datetime | None = None
+
+    class Config:
+        from_attributes = True
+
 
 class UserSubscription(UserSubscriptionBase):
     id: int
+    user_id: int
+    subscription_id: int
+    stripe_customer_id: str
+    stripe_subscription_id: str
+    status: str
+    start_date: datetime
+    end_date: datetime | None = None
     createdOn: datetime
     updatedOn: datetime | None = None
 
